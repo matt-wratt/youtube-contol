bc3792a35e103be322ad420db8551ab22bcbbe2a
Matthew Wratt <matt.wratt@trineo.co.nz>
2015-10-22 14:23:41 +1300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: {
+    popup: './popup.html',
+    manifest: './manifest.json',
+    icon: './icon.png',
+    application: './lib/application.js',
+    background: './lib/background.js'
+  },
+  output: {
+    path: __dirname + '/youtube-control',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader') },
+      { test: /\.(html|json|png)$/, loader: "file?name=[name].[ext]" }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json']
+  },
+  plugins: [
+    new ExtractTextPlugin("application.css")
+  ]
+}
